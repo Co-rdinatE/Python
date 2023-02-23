fc415213e9fbb5fec15b2f9052dcf34ad176106e
Co_rdinatE <minh100107@gmail.com>
2023-02-23 16:04:39 +0700

    asdsa

---
+++ urmum.py
@@ -0,0 +1,18 @@
+print("ur mum") #literally print
+i = 1 #Integer 
+j = "ur mum" #String
+intput = input() #Input, literally
+
+if (i == 1) :
+    print("")
+elif (i != 1):
+    print("")
+else:
+    print("")
+
+
+
+#EXCECISES:
+a = int(input("INPUT A: "))
+b = int(input("INPUT B: "))
+print ("SUM: ", a+b)
